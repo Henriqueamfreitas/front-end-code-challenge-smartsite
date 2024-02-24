56e30c57026ba52dcbcb850d4bcf55ced393c13c
Henrique <you@example.com>
2024-02-24 08:40:10 -0300

    lógica form pronta

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,36 +1,52 @@
 import clock from "../../../_material/images/icon-hour.png"
 import { StyledForm } from "../Form/style.js"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
 
 export const Form = () => {
-    return(
-        <StyledForm>
-          <div>
-            <img src={clock} alt="" />
-            <h2>Horário</h2>
-          </div>
-          <p>Qual período quer treinar?</p>
-          <div>
-            <input type="radio" id="morning" name="period"/>
-            <label htmlFor="morning">Manhã 06:00 às 12:00</label>
-          </div>
-          <div>
-            <input type="radio" id="afternoon" name="period"/>
-            <label htmlFor="afternoon">Tarde 12:01 às 18:00</label>
-          </div>
-          <div>
-            <input type="radio" id="night" name="period"/>
-            <label htmlFor="night">Noite 18:01 às 23:00</label>
-          </div>
+  const { register, handleSubmit, reset } = useForm()
+  const [formData, setFormData] = useState({
+    period: "",
+    closedUnits: false,
+  })
+  const submit = (data) => {
+    setFormData(data)
+    reset()
+  }
 
-          <div>
-            <input type="checkbox" id="closedUnits" name="closedUnits"/>
-            <label htmlFor="closedUnits">Exibir unidades fechadas</label>
-          </div>
+  const handleClear = () => {
+    reset()
+  }
 
-          <p>Resultados encontrados: <span>0</span></p>
+  return (
+    <StyledForm onSubmit={handleSubmit(submit)}>
+      <div>
+        <img src={clock} alt="" />
+        <h2>Horário</h2>
+      </div>
+      <p>Qual período quer treinar?</p>
+      <div>
+        <input type="radio" id="morning" value={"morning"} {...register("period")}/>
+        <label htmlFor="morning">Manhã 06:00 às 12:00</label>
+      </div>
+      <div>
+        <input type="radio" id="afternoon" value={"afternoon"} {...register("period")} />
+        <label htmlFor="afternoon">Tarde 12:01 às 18:00</label>
+      </div>
+      <div>
+        <input type="radio" id="night" value={"night"} {...register("period")} />
+        <label htmlFor="night">Noite 18:01 às 23:00</label>
+      </div>
 
-          <button>Encontrar unidade</button>
-          <button>Limpar</button>
-        </StyledForm>
-    )
+      <div>
+        <input type="checkbox" id="closedUnits" {...register("closedUnits")} />
+        <label htmlFor="closedUnits">Exibir unidades fechadas</label>
+      </div>
+
+      <p>Resultados encontrados: <span>0</span></p>
+
+      <button type="submit">Encontrar unidade</button>
+      <button type="button" onClick={handleClear}>Limpar</button>
+    </StyledForm>
+  )
 }
