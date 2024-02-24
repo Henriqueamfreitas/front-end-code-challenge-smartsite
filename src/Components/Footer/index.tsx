3cd2f29a87d9177816ed0959ee8e7dab47575d88
Henrique <you@example.com>
2024-02-23 21:36:21 -0300

    footer pronto

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,7 +1,11 @@
+import { StyledFooter } from "../Footer/style.js"
+import logo from "../../../_material/images/logo.svg"
+
 export const Footer = () => {
     return(
-        <footer>
-            <h1>FOOTER</h1>
-        </footer>
+        <StyledFooter>
+          <img src={logo} alt="" />
+          <p>Todos os direitos reservados - 2020</p>
+        </StyledFooter>
     )
 }
---
+++ src/Components/Footer/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  background-color: var(--dark-grey);
+  color: var(--light-grey);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 30vh;
+  img{
+    width: 70px;
+    height: 50px;
+  }
+  p{
+    font-size: .75rem;
+    font-weight: 100;
+  }
+`
